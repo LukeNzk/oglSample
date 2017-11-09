6ccb92bd24f39427c064a33fda0a0c3ce7d5937f
Luke Nizik <elejdor@gmail.com>
2017-11-09 11:20:39 +0100

    + project

---
+++ dev/src/engine/private/renderer.cpp
@@ -0,0 +1,9 @@
+#include "renderer.h"
+
+#include <stdio.h>
+#include <iostream>
+
+void Renderer::Draw()
+{
+	std::printf( "draw\n" );
+}
---
+++ dev/src/game/mainGame.cpp
@@ -0,0 +1,9 @@
+#include "..\..\engine\public\renderer.h"
+#include <stdlib.h>
+
+void main()
+{
+	Renderer rend;
+	rend.Draw();
+	system( "pause" );
+}
